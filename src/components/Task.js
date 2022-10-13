1a768732e93756678f66c49364893a16ca269b1b
Misha Tuesday <mishatuesday@gmail.com>
2022-10-13 18:31:48 -0400

    delete buttons working

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Task() {
+function Task(props) {
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{props.category}</div>
+      <div className="text">{props.task}</div>
+      <button className="delete" onClick={e => props.onDelete(props.task)}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+// import { TASKS } from "../data";
+import Task from './Task'
 
-function TaskList() {
+
+function TaskList(props) {
+
+  const theList = props.tasks.map((task, index) => (
+  <Task key={index} task={task.text} category={task.category} onDelete={props.onDelete} />
+  ))
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+        {theList}
     </div>
   );
 }
