53a63e3604e4e04d40e6fbb8976d3332184b89d9
Misha Tuesday <mishatuesday@gmail.com>
2022-10-14 16:40:13 -0400

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,30 @@ import { CATEGORIES, TASKS } from "../data";
 function App() {
   const [stateTasks, setStateTasks] = useState(TASKS)
   const [selectedCategory, setSelectedCategory] = useState("All")
+  const [newTaskName, setNewTaskName] = useState("")
+  const [newTaskCategory, setNewTaskCategory] = useState("Misc")
+
   function handleDelete(name) {
     const revisedList = stateTasks.filter(task => task.text !== name)
     setStateTasks(revisedList)
   }
 
-  
+  function submitForm(e) {
+    e.preventDefault()
+    setStateTasks([
+      ...stateTasks,
+      {text: newTaskName,
+      category: newTaskCategory}
+    ])
+    setNewTaskName("")
+  }
 
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter categories={CATEGORIES} categoryClick={setSelectedCategory} selectedCategory={selectedCategory}/>
-      <NewTaskForm />
       <TaskList tasks={stateTasks} onDelete={handleDelete} selectedCategory={selectedCategory}/>
+      <NewTaskForm submitForm={submitForm} categories={CATEGORIES} newTaskName={newTaskName} setNewTaskName={setNewTaskName} newTaskCategory={newTaskCategory} setNewTaskCategory={setNewTaskCategory} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, newTaskName, setNewTaskName, setNewTaskCategory, submitForm}) {
+  const settableCategories = categories.filter(category => (category !== "All"))
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={e => submitForm(e)}>
+      <h3>Add a New Task:</h3><br />
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={newTaskName} onChange={e => setNewTaskName(e.target.value)}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={e => setNewTaskCategory(e.target.value)}>
+          {settableCategories.map(category => (
+            <option key={category} value={category}>{category}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
